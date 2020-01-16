a38d3523b217201912983512586061cdf7f417a4
Pablo Hugen <pablohuggem@gmail.com>
2020-01-16 17:58:07 -0300

    Import config.h
    Mainly theme modifications to match pywal theme.

---
+++ config.h
@@ -0,0 +1,34 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+//cores do pywal
+#define PYWAL 0
+
+#if PYWAL
+#include "/home/pablo951_br/.cache/wal/colors-wal-dmenu.h"
+#else
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#c4c5c6", "#15181c" },
+	[SchemeSel] = { "#c4c5c6", "#1a3773" },
+	[SchemeOut] = { "#c4c5c6", "#8179a9" },
+};
+#endif
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+//static const char *fonts[] = {
+//	"monospace:size=10"
+//};
+static const char *fonts[] = {
+	"terminus:size=12"
+	//"envypn:size=9"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
