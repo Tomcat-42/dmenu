7b22edab385e24fe5fc1b955633b8312703724c6
Pablo Hugen <pablohuggem@gmail.com>
2021-08-14 14:05:38 -0300

    Add emoji support

--- config.h
+++ config.h
@@ -43,6 +43,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#FFFFFF", "#1e1f26" },
 	[SchemeSel] = { "#bd93f9", "#1e1f26" },
+	[SchemeSelHighlight] = { "#ff79c6", "#1e1f26" },
+	[SchemeNormHighlight] = { "#ff79c6", "#1e1f26" },
 	[SchemeOut] = { "#1e1f26", "#bd93f9" },
 };
 
@@ -60,7 +62,9 @@ static const char *fonts[] = {
     //"terminus:size=12"
     //"envypn:size=9"
     //"GohuFont:size=12"
-    "Iosevka:size=9:antialias=false:autohint=false"};
+    "Iosevka Nerd Font:size=9:antialias=false:autohint=true",
+	  "EmojiOne:pixelsize=12:antialias=true:autohint=true",
+};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
