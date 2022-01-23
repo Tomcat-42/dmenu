af7b7f78b07e631d7e0feb59958442fa6f14e8fc
Pablo Hugen <pablohuggem@gmail.com>
2022-01-23 15:17:31 -0300

    minor changes

--- config.h
+++ config.h
@@ -53,6 +53,8 @@ static const char *colors[SchemeLast][2] = {
 // Fuzzy match patch
 static int fuzzy = 1;
 
+static int centered = 0;/* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 // static const char *fonts[] = {
